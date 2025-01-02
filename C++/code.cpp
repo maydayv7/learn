2331e5412a8e95984b44c4217c5ebedb7b3e81be
maydayv7 <maydayv7@gmail.com>
2025-01-02 11:08:30 +0530

    feat: Add C++ sample

---
+++ C++/code.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+
+void perf()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+void solve()
+{
+}
+
+int main()
+{
+    // perf();
+    ll loop;
+    cin >> loop;
+    while (loop--)
+    {
+        solve();
+    }
+}
