2ec2e926c163fdf95f14f42783a618e23fe80201
maydayv7 <maydayv7@gmail.com>
2025-01-12 23:45:07 +0530

    feat: Update

--- C++/code.cpp
+++ C++/code.cpp
@@ -1,23 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define pb push_back
+typedef long long ll;
+typedef vector<ll> vl;
 
-void perf()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-}
+#define F first
+#define S second
+#define PB push_back
+#define FOR(i, a, b) for (ll i = a; i <= b; i++)
 
 void solve()
 {
-    cout << "Answer" << "\n";
+    ll n;
+    cin >> n;
+
+    FOR(i, 0, n - 1)
+    cout << i << "\n";
 }
 
 int main()
 {
-    // perf();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     ll loop;
     cin >> loop;
     while (loop--)
