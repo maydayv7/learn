179cc18aa4363d35f6844def6610c3773d42795b
maydayv7 <maydayv7@gmail.com>
2025-07-27 14:21:03 +0530

    feat: Update C++ Template

--- C++/code.cpp
+++ C++/code.cpp
@@ -3,32 +3,34 @@
 #include "debug.h"
 #endif
 
+#define int long long
 using namespace std;
 
-typedef long long ll;
-typedef vector<ll> vl;
-
+#define PB push_back
 #define F first
 #define S second
-#define PB push_back
-#define FOR(n) for (ll i{0}; i < n; i++)
+
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
 
 void solve()
 {
-    ll n;
+    int n;
     cin >> n;
     deb(n);
 
-    FOR(n)
-    cout << i << "\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << endl;
+    }
 }
 
-int main()
+signed main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    ll loop;
+    int loop;
     cin >> loop;
     while (loop--)
         solve();
