987504a3b115e933cf6e585b32e8e6580ff83f22
V7 <maydayv7@gmail.com>
2026-01-10 19:00:34 +0530

    feat(LC): 0540-single-element-in-a-sorted-array
    Difficulty: Medium
    Solved in .cpp
    Time: 0 ms (100%)
    Space: 26.1 MB (66.89%)

---
+++ LeetCode/Medium/0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array-01-10-2026-19-00-33.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (m % 2 == 01)
+                m--;
+
+            if (nums[m] == nums[m + 1])
+                l = m + 2;
+            else
+                r = m;
+        }
+        return nums[l];
+    }
+};
